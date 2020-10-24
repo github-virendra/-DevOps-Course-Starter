60ea985fb19f535730e72c130ed0f80eb313b6be
Virendra S <virendra.sankpal@credit-suisse.com>
2020-10-24 21:29:33 +0100

    Changes for Module-1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from todo_app.data import session_items 
 
 from todo_app.flask_config import Config
 
@@ -6,9 +7,27 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+#@app.route('/')
+#def index():
+#   return render_template('index.html',items=session_items.get_items())
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+       session_items.add_item(request.form.get('Title'))
+
+
+    return render_template('index.html',items=session_items.get_items())
+
+@app.route('/result', methods=['POST'])
+def create():
+    print("in Create function" + str(request.form.get('Title')))
+    item  = request.form.get('Title')
+    if item != '':
+        session_items.add_item(item)
+    return redirect(url_for('index'))
+ 
 
 
 if __name__ == '__main__':
